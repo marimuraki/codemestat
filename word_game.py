9c057b13eeb986b759fd8053f4c3c324ff02760c
Mari Muraki <marimuraki@gmail.com>
2014-01-22 17:47:11 -0800

    added list append & join

--- word_game.py
+++ word_game.py
@@ -1,11 +1,13 @@
 
 print "Enter a word to create a sentence. Enter ! or . to end it."
-
+sentence = []
 while True:
-    x_new = raw_input("What's your word? ")
-    if x_new != "!" and x_new != ".":
-        print x_new
+    word = raw_input("What's your word? ")
+    if word != "!" and word != ".":
+        print word
+        sentence.append(word)
     else:
+        print ' '.join(sentence) + word
         break
         
     
