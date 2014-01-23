dc4da7ee6640848213102d6d553a77318dfd5096
Mari Muraki <marimuraki@gmail.com>
2014-01-22 17:15:46 -0800

    added ValueError exception

--- number_game.py
+++ number_game.py
@@ -5,21 +5,24 @@ count = 0
 
 print "The game begins! Enter an integer ... if it's > or <, I'll tell you and if not, I'll quit (I'll give you 10 trys!)."
 
-while True:
-    x_new = int(raw_input("What's your number? "))
-    if count == 0:
-        print "Your first number!"
-    else:
-        if x_new > x:
-            print " -> it's bigger than the last!"
-        elif x_new < x:
-            print " ->  it's smaller than the last!"
+try:
+    while True:
+        x_new = int(raw_input("What's your number? "))
+        if count == 0:
+            print "Your first number!"
         else:
-            print " -> no change! I'll exit now."
+            if x_new > x:
+                print " -> it's bigger than the last!"
+            elif x_new < x:
+                print " ->  it's smaller than the last!"
+            else:
+                print " -> no change! I'll exit now."
+                break
+            
+        x = x_new
+        count += 1
+        if count > max_tries:
+            print "too many tries ..."
             break
-        
-    x = x_new
-    count += 1
-    if count > max_tries:
-        print "too many tries ..."
-        break
+except ValueError:
+    print "Not an integer!"
